446961a0821d750992e2a3c0b372aa46623e54cc
Graphic <graphic@SOD-SJH-603-20M.local>
2018-04-19 20:01:17 -0400

    Ghibli database

--- Final-Project/src/App.js
+++ Final-Project/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   searchAnimeFilmByTitle(title) {
-    const api = 'curl https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49""';
+    const api = 'curl https://ghibliapi.herokuapp.com/films/""';
     const animespecies = this.state.animespecies;
 
 
